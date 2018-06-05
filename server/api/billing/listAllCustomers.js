72fd204f4fb0889adfc512b0b653a7a890feeb64
loainagati <loainagati@gmail.com>
2018-06-05 21:27:01 +0200

    removed all files in billing folder except for billing.controller

--- server/api/billing/listAllCustomers.js
+++
@@ -1,7 +0,0 @@
-var stripe = require('stripe')('sk_test_cY03qigypQzbZVaJGbw9n3TO');
-stripe.customers.list(
-    { limit: 3 },
-    function(err, customers) {
-        console.log(customers)
-    }
-);
